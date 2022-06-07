cf7c8db6e8a005eaf523c68fe97fbd06662924c5
quan.lh <quanlh@tokyotechlab.com>
2022-06-08 00:59:13 +0700

    init feature copy to clipboard

---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+export const enableHostClipboardFeature: string[] = [
+    'stackoverflow.com',
+    'stackexchange.com',
+    'superuser.com',
+    'serverfault.com',
+    'askubuntu.com',
+    'mathoverflow.net',
+    'math.stackexchange.com',
+    'codereview.stackexchange.com',
+    'programmers.stackexchange.com',
+    'stackapps.com',
+    'viblo.asia',
+    'www.npmjs.com',
+]
+
+export const excludeHostClipboardFeature: string[] = [
+    'w3schools.com'
+]
